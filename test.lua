4a1add97366ec95186a90d8a5a6e6c58a1427751
MaximoMora <maxmora401@gmail.com>
2023-07-11 02:30:06 -0400

    new things

---
+++ test.lua
@@ -0,0 +1,21 @@
+
+local name = "max"
+print("my names is " .. name .. "")
+
+local table = [[
+    HOla 
+    max 
+                  4
+))))
+]]
+
+print(table)
+
+
+local operation = ((5^2)+2)
+
+local fruit = "manzana "
+local vegetal = "zanahoria"
+print(fruit .. vegetal)
+
+print(string.byte)
